0d80eb6ba08018e14ebc5c8c91fccf9a42cad6a1
AnthonyRozdobudko <anton.rozdobudko@assetbook.com>
2021-03-24 09:46:06 +0200

    s

---
+++ routes/shop.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const adminData = require('./admin')
+
+router.get('/', (req, res, next) => {
+    const products = adminData.products;
+    res.render('shop', {prods: products, docTitle: 'shop'});
+});
+
+module.exports = router;
